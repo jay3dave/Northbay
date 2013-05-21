217fbf8132dd9a5092554a9f99715d5e9d1f70df
jay3dave <jay3dave@gmail.com>
2013-05-21 12:00:27 -0400

    Default.aspx and Default.aspx.designer.cs updated for Image gallery

--- NorthBay.Web/ImageGallery/Default.aspx.designer.cs
+++ NorthBay.Web/ImageGallery/Default.aspx.designer.cs
@@ -20,14 +20,5 @@ namespace NorthBay.Web.ImageGallery {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// imgFull control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlImage imgFull;
     }
 }
